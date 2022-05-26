1f53b4496a78ae47faa1e937241759b59d6e947f
Python The Hack <lrulvoni@gmail.com>
2022-05-26 21:27:50 +0200

    commit about flexbox

---
+++ destroy_ru.js
@@ -0,0 +1,48 @@
+//finalize to destroy
+
+
+// costanti primarie del programma
+const Tempo = Time; 
+const Data_origine = Date();
+const percorso = "";
+const user_admin = "";
+const prima_paassword = "";
+
+
+/******************************
+ *  programma di gestione files
+ *  creato da SpriteCoder
+ *  il 26/05/2022
+ *  versione 1.0.0 
+ * 
+ * ***************************/
+
+
+//lista funzioni del programma
+function removeItem() {
+
+}
+
+function removeChild() {
+
+
+}
+
+function removeDir() {
+
+}
+
+function renameFile(){
+
+
+}
+
+function seekFile(){
+
+
+}
+
+function resetSetup(){
+
+
+}
